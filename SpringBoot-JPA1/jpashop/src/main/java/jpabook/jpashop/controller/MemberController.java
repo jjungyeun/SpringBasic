6ccd88f022d06ac326b406c5c03bcdf443460064
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-01-03 00:08:51 +0900

    [스프링 부트 JPA 1] - 섹션 7. 회원 등록까지

---
+++ SpringBoot-JPA1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(
+            @Valid MemberForm memberForm,
+            BindingResult result    // @Valid 다음에 BindingResult 객체가 있으면 오류를 튕기지 않고 얘가 받아줌
+    ){
+
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
