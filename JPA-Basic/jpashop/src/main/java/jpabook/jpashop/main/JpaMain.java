4523eec3a2b65da12a34eb54fab5c0769201655c
JungYeunWon <jungyeun_won@tmax.co.kr>
2022-01-20 14:24:17 +0900

    [JPA 기본편] 섹션 4. 엔티티 매핑 - 실전 예제 1

---
+++ JPA-Basic/jpashop/src/main/java/jpabook/jpashop/main/JpaMain.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.main;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // EMF는 애플리케이션 실행 단위마다 하나만 생성해야 함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 트랜잭션 단위로 뭔가를 수행할 때는 EM을 생성하여 사용
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        try {
+
+
+            transaction.commit();
+        }
+        catch (Exception e){
+            transaction.rollback();
+        }
+        finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
