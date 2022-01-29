a991b6b734161b81b9d5aec1752085baee041aa7
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-01-29 18:35:45 +0900

    [JPA 기본편] 섹션 10. JPQL 기본 문법 2

--- JPA-Basic/jpql/src/main/java/jpqltest/JpaMain.java
+++ JPA-Basic/jpql/src/main/java/jpqltest/JpaMain.java
@@ -1,5 +1,7 @@
 package jpqltest;
 
+import jpqltest.dto.MemberDto;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -12,14 +14,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setAge(26);
-            member.setName("WJY");
-            em.persist(member);
+            Member member0 = new Member();
+            member0.setAge(26);
+            member0.setName("member1");
+            em.persist(member0);
+
+            Team team = new Team();
+            team.setName("team1");
+            team.addMember(member0);
+            em.persist(team);
+
+            Member member2 = new Member();
+            member2.setAge(24);
+            member2.setName("member2");
+            em.persist(member2);
+
+            Team team2 = new Team();
+            team2.setName("team2");
+            team2.addMember(member2);
+            em.persist(team2);
+
+            em.flush();
+            em.clear();
 
             // 반환 타입이 명확하면 TypedQuery 클래스 사용
             TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.name from Member m where m.id = 1", String.class);
+            TypedQuery<String> query2 = em.createQuery("select m.name from Member m where m.name = 'member1'", String.class);
             // 반환 타입이 명확하지 않은 경우 Query 클래스 사용
             Query query3 = em.createQuery("select m.name, m.age from Member m");
 
@@ -34,6 +54,44 @@ public class JpaMain {
             query4.setParameter("name", "WJY");
             Member WJY = query4.getSingleResult();
             System.out.println("WJY = " + WJY);
+
+            // 프로젝션 -  여러 값 조회 시 new 명령어로 조회하는 방법
+            List<MemberDto> resultList = em.createQuery("select new jpqltest.dto.MemberDto(m.name, m.age) from Member m", MemberDto.class)
+                    .getResultList();
+            for (MemberDto memberDto : resultList) {
+                System.out.println("memberDto = " + memberDto);
+            }
+
+            // 페이징
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(5)
+                    .getResultList();
+            System.out.println("result.size() = " + result.size());
+            for (Member member : result) {
+                System.out.println(member);
+            }
+
+            // 조인
+            String jpql = "select m from Member m inner join m.team t";
+            String jpql2 = "select m from Member m left join m.team t";
+            String jpql3 = "select m, t from Member m, Team t where m.name = t.name";
+            // 연관관계 없는 외부 조인 (멤버 이름 = 팀 이름)인 멤버, 팀 쌍 조회
+            String jpql4 = "select m, t from Member m left join Team t on m.name = t.name";
+            List<Object[]> resultList2 = em.createQuery(jpql4, Object[].class)
+                    .getResultList();
+            for (Object[] objects : resultList2) {
+                System.out.println("objects[0] = " + (Member)objects[0]);
+                System.out.println("objects[1] = " + (Team)objects[1]);
+            }
+
+            // 사용자 정의 함수 사용
+            String jpql5 = "select function('group_concat', m.name) from Member m";
+            List<String> concatResult = em.createQuery(jpql5, String.class)
+                    .getResultList();
+            for (String s : concatResult) {
+                System.out.println("s = " + s);
+            }
 
 
             tx.commit();
--- JPA-Basic/jpql/src/main/java/jpqltest/Team.java
+++ JPA-Basic/jpql/src/main/java/jpqltest/Team.java
@@ -18,6 +18,12 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
+    public void addMember(Member member){
+        this.members.add(member);
+        member.setTeam(this);
+    }
+
+
     public Long getId() {
         return id;
     }
@@ -36,5 +42,13 @@ public class Team {
 
     public void setMembers(List<Member> members) {
         this.members = members;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
