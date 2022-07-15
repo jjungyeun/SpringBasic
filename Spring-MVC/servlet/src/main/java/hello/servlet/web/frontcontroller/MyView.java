90cdba53a0849b8d8565d0d39d5c8acf49bf8063
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-07-15 09:27:40 +0900

    [Spring MVC 1] 섹션 4. View 분리(v2)

---
+++ Spring-MVC/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    // 각 Controller에서 수행했던 공통 부분(View Forwarding)을 대신하는 메소드
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
