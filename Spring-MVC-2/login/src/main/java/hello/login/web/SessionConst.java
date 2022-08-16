91d853de391829e29aef1b9c9a0c6b841853926b
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-08-16 15:30:06 +0900

    [Spring MVC 2] 섹션 6. 로그인 처리 1 - 서블릿 HTTP 세션

---
+++ Spring-MVC-2/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+// 생성할 필요 없는 클래스이기 때문에 abstract class 또는 interface로 정의하는 것이 좋음
+public abstract class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
