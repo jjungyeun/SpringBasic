c04427ac4598fd172d4c8817e05713128960000c
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-08-16 12:42:17 +0900

    [Spring MVC 2] 섹션 6. 로그인 처리 1 - 쿠키 사용

--- Spring-MVC-2/login/src/main/java/hello/login/web/login/LoginController.java
+++ Spring-MVC-2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +28,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if (bindingResult.hasErrors())
             return "login/loginForm";
 
@@ -38,7 +41,24 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // TODO - 로그인 성공 처리
+        // 로그인 성공 처리
+
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키로 설정됨 (브라우저 종료 시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", loginMember.getId().toString());
+        response.addCookie(idCookie);
+
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);    // expired time을 0으로 설정하면 됨
+        response.addCookie(cookie);
     }
 }
