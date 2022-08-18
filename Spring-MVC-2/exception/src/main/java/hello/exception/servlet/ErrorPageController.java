215a718dd58db53204674d1697ba48ed1d221824
jjungyeun <wjddusdl11@ajou.ac.kr>
2022-08-18 10:39:11 +0900

    [Spring MVC 2] 섹션 8. 예외 처리와 오류 페이지 - 서블릿 예외 처리(필터)

---
+++ Spring-MVC-2/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -0,0 +1,40 @@
+package hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import static javax.servlet.RequestDispatcher.*;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+    @RequestMapping("/error-page/404")
+    public String err404(HttpServletRequest request) throws IOException {
+        log.info("ErrorPage 404");
+        printErrorInfo(request);
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String err500(HttpServletRequest request) throws IOException {
+        log.info("ErrorPage 500");
+        printErrorInfo(request);
+        return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request){
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatcherType={}", request.getDispatcherType());
+    }
+}
