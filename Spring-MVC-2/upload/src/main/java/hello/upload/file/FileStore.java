60044c4d6bf05b6c0bfd02bcb2201081f17504ca
jjungyeun <rpgkk99@gmail.com>
2022-09-03 16:51:07 +0900

    [Spring MVC 2] 섹션 11. 파일 업로드 - 파일 업로드 다운로드 서비스 예제

---
+++ Spring-MVC-2/upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,54 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty())
+            return null;
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFilename = createStoreFilename(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFilename)));
+
+        return new UploadFile(originalFilename, storeFilename);
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty())
+                storeFileResult.add(storeFile(multipartFile));
+        }
+        return storeFileResult;
+    }
+
+    private String createStoreFilename(String originalFilename) {
+        String extension = extractExtension(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + extension;
+    }
+
+    private String extractExtension(String originalFilename){
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+    public String getFullPath(String filename){
+        return fileDir + filename;
+    }
+}
