2ecf23f8eb03babd224804b42a361f016a7124c9
jjungyeun <wjyddd@naver.com>
2023-07-26 10:39:54 +0900

    [Spring DB 1] 섹션 1. JDBC 이해 - 조회 기능
    - Member 조회 기능 구현
    - 관련 테스트 코드 작성

--- Spring-DB-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ Spring-DB-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -28,6 +29,33 @@ public class MemberRepositoryV0 {
             throw e;
         } finally {
             close(conn, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) { // 결과 데이터가 있는지 확인
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else { // 결과가 없는 경우
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, rs);
         }
     }
 
--- Spring-DB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ Spring-DB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,27 +1,27 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repo = new MemberRepositoryV0();
 
     @Test
-    public void save() throws SQLException {
-        // given
+    public void crud() throws SQLException {
+        // save
         Member member = new Member("memberV0", 10000);
-
-        // when
         repo.save(member);
 
-        // then
-
-
+        // select
+        Member foundMember = repo.findById(member.getMemberId());
+        assertEquals(member, foundMember); // Member에 @Data 붙어있어서 값비교 가능
     }
 
 }
