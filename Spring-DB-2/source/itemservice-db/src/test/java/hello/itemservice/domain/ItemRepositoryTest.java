90c73e178ce62d64caa79cac1915c1f3b6c6d208
jjungyeun <wjyddd@naver.com>
2024-01-16 13:52:16 +0900

    [Spring DB 2] 섹션 3. 테스트 - @Transactional
    - 테스트에 @Transactional을 통한 트랜잭션 적용

--- Spring-DB-2/source/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ Spring-DB-2/source/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -11,28 +11,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
-    PlatformTransactionManager transactionManager;
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//
+//    TransactionStatus status;
 
-    TransactionStatus status;
-
-    @BeforeEach
-    void init() {
-        // 테스트 시작 시 트랜잭션 시작
-        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
+//    @BeforeEach
+//    void init() {
+//        // 테스트 시작 시 트랜잭션 시작
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
 
     @AfterEach
     void afterEach() {
@@ -41,8 +43,8 @@ class ItemRepositoryTest {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
 
-        // 테스트 종료 시 롤백
-        transactionManager.rollback(status);
+//        // 테스트 종료 시 롤백
+//        transactionManager.rollback(status);
     }
 
     @Test
