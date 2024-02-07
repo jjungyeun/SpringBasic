1cf0f7f356aab49727af12d30d8726d6bf36fbf7
jjungyeun <wjyddd@naver.com>
2024-02-07 11:04:27 +0900

    [Spring 고급편] 섹션 2. ThreadLocal - 스레드 로컬 동기화 적용

---
+++ SpringCore-Advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    public void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // A가 완전히 끝나고 B 시작하는 케이스. 동시성 이슈 X
+        sleep(100); // A가 끝나기 전에 B 시작하는 케이스. 동시성 이슈 O
+        threadB.start();
+        sleep(3000); // 메인 스레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
