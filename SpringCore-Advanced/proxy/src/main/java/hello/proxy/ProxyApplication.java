983161c03281324efdefac8e54cd6ee70a570c54
jjungyeun <wjyddd@naver.com>
2024-02-23 16:14:38 +0900

    [Spring 고급편] 섹션 4. 프록시 - 인터페이스 기반 프록시 적용

--- SpringCore-Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore-Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,11 +2,16 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import({InterfaceProxyConfig.class})
 
 // Config 클래스가 컴포넌트 스캔 되지 않도록 스캔 패키지 지정
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
@@ -16,4 +21,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
