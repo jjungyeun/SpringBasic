88f92c03194123f248591ad99cd20a26ee428dff
jjungyeun <wjyddd@naver.com>
2024-02-24 16:39:52 +0900

    [Spring 고급편] 섹션 4. 프록시 - 구체 클래스 기반 프록시 적용

---
+++ SpringCore-Advanced/proxy/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,33 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+    private final LogTrace logTrace;
+
+    // 부모 객체를 사용하지 않고 target 객체를 직접 받아서 사용하기 떄문에 super에 연결해줄 필요 없음
+    // -> 인터페이스 없이 구체 클래스를 직접 상속하는 것의 단점!
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.request()");
+            // target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
