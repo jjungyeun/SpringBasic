6ff740b5882877d8163dbb5ec1f5d8adb6eb05e8
jjungyeun <wjyddd@naver.com>
2024-02-26 14:26:37 +0900

    [Spring 고급편] 섹션 5. 동적 프록시 - CGLIB 예제

---
+++ SpringCore-Advanced/proxy/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,31 @@
+package hello.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        // CGLIB에서는 성능상 Method보다 MethodProxy를 사용하는 것이 권장된다.
+        Object result = methodProxy.invoke(target, args);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료. resultTime={}", resultTime);
+        return result;
+    }
+}
