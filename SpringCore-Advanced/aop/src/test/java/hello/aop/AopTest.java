4692434a904c83edbb6e78a1736ca7e308282124
jjungyeun <wjyddd@naver.com>
2024-03-04 15:41:31 +0900

    [Spring 고급편] 섹션 10. 스프링 AOP - @Aspect 예제

--- SpringCore-Advanced/aop/src/test/java/hello/aop/AopTest.java
+++ SpringCore-Advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,19 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
